1f90435a49733458ae9b25a5e56b0be8e3d5a267
andrzto <60622479+andrzto@users.noreply.github.com>
2024-09-17 20:09:11 -0500

    MyFristComponent

---
+++ src/components/MyFristComponent.jsx
@@ -0,0 +1,16 @@
+
+export const MyFristComponent = () => {
+  return (
+    <>
+        <div>MyFristComponent</div>
+        <h1>Temas de React</h1>
+        <ul>
+            <li>Componentes</li>
+            <li>Eventos</li>
+            <li>Estados hooks</li>
+            <li>Props</li>
+        </ul>
+    </>
+  )
+}
+
