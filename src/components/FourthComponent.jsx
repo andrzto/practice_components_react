a378ef8065eb448d9fccc5e069244b7208bb9f05
andrzto <60622479+andrzto@users.noreply.github.com>
2024-09-20 21:31:00 -0500

    FinishfourthComponent

--- src/components/FourthComponent.jsx
+++ src/components/FourthComponent.jsx
@@ -2,52 +2,75 @@
 export const FourthComponent = () => {
 
   const handleClicked = (e, name) => {
-    alert("Has hecho clic en el botón"  +  name);
+    alert("Has hecho click en el botón " + name);
   }
 
   const handleDoubleClicked = () => {
-    alert("Haz hecho doble clic");
+    alert("Has hecho doble click");
   }
 
   const handleMouseEnter = (e) => {
-    alert("Haz ENTRADO a la caja");
+    alert("Has ENTRADO a la caja");
   }
 
   const handleMouseLeave = (e) => {
-    alert("Haz SALIDO de la caja");
+    alert("Has SALIDO de la caja");
+  }
+
+  const handleMouse = (e, action) => {
+    alert(`Has ${action} la caja`);
+  }
+
+  const handleInsideInput = (e) => {
+    console.log("Estas dentro del input, escribe tu nombre!!");
+  }
+
+  const handleOutsideInput = (e)=> {
+    console.log("Estás fuera del input, Adiós!!")
   }
 
   return (
     <div>
-        <h1>FourComponent</h1>
-        {/* Evento Click*/}
-        <div>
-        <button 
-        className= "btn btn-danger" 
-        onClick={ () => {
-          alert("hola, soy un evento click");
-        }}
+      <h1>FourComponent</h1>
+      {/* Evento Click */}
+      <div>
+        <button
+          className="btn btn-danger"
+          onClick={ () => {
+            alert("Hola, soy un evento click");
+          }}
         >Haz clic!!
         </button>
-        </div>
+      </div>
       <div>
-        <button className= "btn btn-success " 
-          onClick={e => handleClicked(e, "Andrés")}>
-          ¡Aqui también haz clic!
+        <button className="btn btn-success"
+          onClick={ e => handleClicked(e, "Inés") }>
+          ¡Aquí también haz click!
         </button>
       </div>
-      <button className= "btn btn-info"
-         onDoubleClick={ handleDoubleClicked }
-         >
-          Haz doble click!!
-       </button>
+      <button className="btn btn-info"
+        onDoubleClick={ handleDoubleClicked }
+      >
+        Haz Doble Click!!
+      </button>
       <div id="box1"
+        className="mt-4"
         onMouseEnter={ e => handleMouseEnter(e)}
         onMouseLeave={ e => handleMouseLeave(e)}
       >
         <p>Pasa el mouse por encima!!</p>
       </div>
+      <div id="box2" className="mt-4"
+        onMouseEnter={ e => handleMouse(e, "entrado a")}
+        onMouseLeave={ e => handleMouse(e, "salido de") }
+      >
+      </div>
+      <div className="mt-4">
+        <input type="text"
+          onFocus={ e => handleInsideInput(e)}
+          onBlur={ e => handleOutsideInput(e)}
+        />
+      </div>
     </div>
-    
   )
 }
